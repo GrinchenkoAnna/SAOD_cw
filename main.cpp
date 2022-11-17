aaf78061964de2dd97d1f4268a584e4e9da0f0f3
GrinchenkoAnna <grinchenkoanna@inbox.ru>
2022-11-18 00:56:12 +0700

    Added index array & poss. to display 20 records

--- main.cpp
+++ main.cpp
@@ -22,14 +22,41 @@ int main()
     j = fread((base*)record, sizeof(base), 4000, in);
     fclose(in);
 
+    int b[4000];
     for (int i = 0; i < 4000; i++)
     {
-        cout << record[i].full_name_depositor << endl;
-        cout << record[i].deposit_sum << endl;
-        cout << record[i].deposit_date << endl;
-        cout << record[i].full_name_advocate << endl << endl;
+        b[i] = i;
     }
-    cout << sizeof(record) << endl;
+
+    int k = 0;
+    char ch = 'y';
+
+    while (k < 200 && ch == 'y')
+    {
+        for (int i = 20*k; i < 20*(k + 1); i++)
+        {
+            cout << b[i] + 1 << ") " << endl;
+            cout << record[i].full_name_depositor << endl;
+            cout << record[i].deposit_sum << endl;
+            cout << record[i].deposit_date << endl;
+            cout << record[i].full_name_advocate << endl << endl;
+        }
+
+        cout << "Do you want to output the following 20 database records? (y/n) ";
+        cin >> ch;
+        cout << endl;
+
+        if (ch != 'n' && ch != 'y')
+        {
+            cout << "Unable to recognize the command" << endl;
+            cout << "Do you want to output the following 20 database records? (y/n) ";
+            cin >> ch;
+        }
+
+        if (ch == 'n') break;
+
+        k++;
+    }
 
     return 0;
 }
