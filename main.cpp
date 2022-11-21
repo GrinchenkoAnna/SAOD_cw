08c3dbe85de5b606e4d5e1e6323f8747c6cd4008
GrinchenkoAnna <grinchenkoanna@inbox.ru>
2022-11-21 20:08:57 +0700

    Added class

--- main.cpp
+++ main.cpp
@@ -3,60 +3,114 @@
 
 using namespace std;
 
-struct base
+const unsigned int LD_NAME = 30;
+const unsigned int L_DATE = 10;
+const unsigned int LA_NAME = 22;
+const unsigned int MAX = 4000;
+
+class base
 {
-    char full_name_depositor[30];
+private:
+    char full_name_depositor[LD_NAME];
     unsigned short deposit_sum;
-    char deposit_date[10];
-    char full_name_advocate[22];
+    char deposit_date[L_DATE];
+    char full_name_advocate[LA_NAME];
+
+    static unsigned int n; //current number of records
+    static base* arr[]; //array of pointers to the class
+    static base** index_arr[];
+
+public:
+    void putdata()
+    {
+        cout << full_name_depositor << endl;
+        cout << deposit_sum << endl;
+        cout << deposit_date << endl;
+        cout << full_name_advocate << endl << endl;
+    }
+
+    static void read();
+    static void display();
+    static void index();
 };
+
+unsigned int base::n;
+base* base::arr[MAX];
+//base** base::index_arr[MAX];
+
+void base::read()
+{
+    ifstream in;
+    in.open("testBase3.dat", ios::binary);
+    if (!in)
+    {
+        cout << "Unable to open the file!\n"; return;
+    }
+    else cout << "The file is open\n";
+
+    n = 0;
+
+    while(true)
+    {
+        arr[n] = new base;
+        in.read((char*)arr[n], sizeof(base));
+        if (in.eof()) break;
+        n++;
+    }
+
+    cout << "Reading " << n << " records\n";
+}
+
+void base::display()
+{
+    for (int i = 0; i < n; i++)
+    {
+        int k = 0;
+        char ch = 'y';
+
+        while (k < 200 && ch == 'y')
+        {
+            for (int i = 20*k; i < 20*(k + 1); i++)
+            {
+            cout << (i + 1) << ") " << endl; //print the number of record
+            arr[i] -> putdata();
+            }
+
+            cout << "Do you want to output the following 20 database records? (y/n) ";
+            cin >> ch;
+            cout << endl;
+
+            while (ch != 'n' && ch != 'y')
+            {
+                cout << "\nUnable to recognize the command\n" << endl;
+                cout << "Do you want to output the following 20 database records? (y/n) ";
+                cin >> ch;
+            }
+
+            if (ch == 'n') break;
+
+            k++;
+         } break;
+    }
+}
+
+
+/*///??
+void base::index()
+{
+    for (int i = 0; i < n; i++)
+    {
+        index_arr[i] = &arr[i];
+        //cout << i+1 << endl;
+    }
+}*/
+
 
 int main()
 {
-    FILE *in;
-    in = fopen("testBase3.dat", "rb");
-
-    base record[4000] = {0};
-
-    int j = 0;
-    j = fread((base*)record, sizeof(base), 4000, in);
-    fclose(in);
-
-    int b[4000];
-    for (int i = 0; i < 4000; i++)
-    {
-        b[i] = i;
-    }
-
-    int k = 0;
-    char ch = 'y';
-
-    while (k < 200 && ch == 'y')
-    {
-        for (int i = 20*k; i < 20*(k + 1); i++)
-        {
-            cout << b[i] + 1 << ") " << endl;
-            cout << record[i].full_name_depositor << endl;
-            cout << record[i].deposit_sum << endl;
-            cout << record[i].deposit_date << endl;
-            cout << record[i].full_name_advocate << endl << endl;
-        }
-
-        cout << "Do you want to output the following 20 database records? (y/n) ";
-        cin >> ch;
-        cout << endl;
-
-        if (ch != 'n' && ch != 'y')
-        {
-            cout << "Unable to recognize the command" << endl;
-            cout << "Do you want to output the following 20 database records? (y/n) ";
-            cin >> ch;
-        }
-
-        if (ch == 'n') break;
-
-        k++;
-    }
+    base::read();
+    base::display();
+    //base::index();
 
     return 0;
 }
